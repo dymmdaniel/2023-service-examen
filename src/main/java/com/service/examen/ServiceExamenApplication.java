c11fc2f892017ccfa451b6990ea340c4cc7d3933
dymmdaniel <64989525+dymmdaniel@users.noreply.github.com>
2023-02-27 09:21:05 -0500

    init commit

---
+++ src/main/java/com/service/examen/ServiceExamenApplication.java
@@ -0,0 +1,13 @@
+package com.service.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceExamenApplication.class, args);
+	}
+
+}
